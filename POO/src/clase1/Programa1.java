2c93179ff3bb2b7fdbd39b3f19e01401b42138bf
miluvalverde <miluvalverde11@gmail.com>
2022-10-22 20:26:32 -0300

    Add src file with first programm in clase 1

---
+++ POO/src/clase1/Programa1.java
@@ -0,0 +1,18 @@
+package clase1;
+
+public class Programa1 {
+
+    public static void main(String[] args) {
+
+        String nombre = "Braulio";
+        String raza = "Criollo";
+        float loQueProduce = 1250.40f;
+        float loQueConsume = 1600.40f;
+
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Déficit: " + (loQueProduce - loQueConsume));
+
+        System.out.println("Hola Mundo");
+    }
+
+}
