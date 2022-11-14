ebca70dd95f5413f1eb09f9968a8cf844ebacb26
miluvalverde <miluvalverde11@gmail.com>
2022-11-14 16:45:54 -0300

    Add class Articulo to file editorial in clase7

---
+++ POO/src/clase7/editorial/Articulo.java
@@ -0,0 +1,19 @@
+package clase7.editorial;
+
+import java.util.Date;
+
+public class Articulo {
+
+    private String titulo;
+    private String tema;
+    private String autor;
+    private Date fecha;
+
+
+    // Constructor
+    public Articulo(String titulo, String tema) {
+        this.titulo = titulo;
+        this.tema = tema;
+    }
+
+}
