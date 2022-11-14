4186bb5c920ba81817485531cb93536ea7a9c308
miluvalverde <miluvalverde11@gmail.com>
2022-11-14 16:53:25 -0300

    Add exercise description

--- POO/src/clase7/pedidos/Pedido.java
+++ POO/src/clase7/pedidos/Pedido.java
@@ -4,6 +4,14 @@ import java.util.Date;
 
 public class Pedido {
 
+    /*Se desea modelar como es el registro de pedidos en un negocio de venta online.
+    Los clientes a los cuales se les pide nombre, teléfono, domicilio, realizan los pedidos
+    mediante una página web del comercio. La información que queda guardada del pedido es
+    fecha del pedido, cliente que lo efectuó, además cada pedido tiene un código. Cada pedido
+    a su vez está compuesto por varios ítems, (los artículos adquiridos). Para el comercio un
+    artículo tiene un nombre y un precio*/
+
+
     // Atributos
     private Date fecha;
     private int codigo;
