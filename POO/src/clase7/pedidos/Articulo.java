9f45240e080a528ca753be417854098e80ba04ab
miluvalverde <miluvalverde11@gmail.com>
2022-11-14 16:49:09 -0300

    Add class Articulo to file pedidos in clase7

---
+++ POO/src/clase7/pedidos/Articulo.java
@@ -0,0 +1,16 @@
+package clase7.pedidos;
+
+public class Articulo {
+
+    // Atributos
+    private String nombre;
+    private double precio;
+
+
+    // Constructor
+    public Articulo(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+    
+}
