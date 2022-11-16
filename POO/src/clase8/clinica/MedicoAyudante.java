9a3438c45e9633865792d99487213f34ee8998c3
miluvalverde <miluvalverde11@gmail.com>
2022-11-15 21:21:55 -0300

    Add class MedicoAyudante file clinica in clase 8

---
+++ POO/src/clase8/clinica/MedicoAyudante.java
@@ -0,0 +1,9 @@
+package clase8.clinica;
+
+public class MedicoAyudante extends Medico{
+
+    // Constructor
+    public MedicoAyudante(int numeroMatricula, String nombre, double importeHonorario) {
+        super(numeroMatricula, nombre, importeHonorario);
+    }
+}
