5005163dfdf89a2c6b9e895c8e65618220af2e68
miluvalverde <miluvalverde11@gmail.com>
2022-11-17 15:35:43 -0300

    Add class Estudiante file colegio in clase 8

---
+++ POO/src/clase8/colegio/Estudiante.java
@@ -0,0 +1,15 @@
+package clase8.colegio;
+
+public class Estudiante extends Persona {
+
+    // Constructor
+    public Estudiante(String nombre, String apellido) {
+        super(nombre, apellido);
+    }
+
+
+    // Métodos
+    public void estudiar() {
+        // Lógica
+    }
+}
