8eab76f07fb9e5e8a88b6a25c33fdbb14e3193f8
miluvalverde <miluvalverde11@gmail.com>
2022-11-17 15:37:02 -0300

    Add class Entrenador file equipo in clase 8

---
+++ POO/src/clase8/equipo/Entrenador.java
@@ -0,0 +1,19 @@
+package clase8.equipo;
+
+public class Entrenador extends Persona{
+
+    // Constructor
+    public Entrenador(String nombre, String apellido) {
+        super(nombre, apellido);
+    }
+
+
+    // Métodos
+    public void diseniarTecnicas() {
+        // Lógica
+    }
+
+    public void dirigirEntrenamiento() {
+        // Lógica
+    }
+}
