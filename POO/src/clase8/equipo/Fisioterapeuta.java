e9e84e3a729639cd6b76c8dfedafd386bd07f146
miluvalverde <miluvalverde11@gmail.com>
2022-11-17 15:37:19 -0300

    Add class Fisioterapeuta file equipo in clase 8

---
+++ POO/src/clase8/equipo/Fisioterapeuta.java
@@ -0,0 +1,23 @@
+package clase8.equipo;
+
+public class Fisioterapeuta extends Persona {
+
+    // Constructor
+    public Fisioterapeuta(String nombre, String apellido) {
+        super(nombre, apellido);
+    }
+
+
+    // Métodos
+    public void controlMedico() {
+        // Lógica
+    }
+
+    public void registrarProgresoRecuperacionLesion() {
+        // Lógica
+    }
+
+    public void hacerMasajes() {
+        // Lógica
+    }
+}
