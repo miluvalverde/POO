db5bcf1ac520030406a1f7cf5fb896c1b8210048
miluvalverde <miluvalverde11@gmail.com>
2022-11-17 15:38:06 -0300

    Add class Presidente file equipo in clase 8

---
+++ POO/src/clase8/equipo/Presidente.java
@@ -0,0 +1,19 @@
+package clase8.equipo;
+
+public class Presidente extends Persona {
+
+    // Constructor
+    public Presidente(String nombre, String apellido) {
+        super(nombre, apellido);
+    }
+
+
+    // Métodos
+    public void supervisar() {
+        // Lógica
+    }
+
+    public void actividadesAdministrativas() {
+        // Lógica
+    }
+}
