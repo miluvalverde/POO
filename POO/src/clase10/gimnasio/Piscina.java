9f7e4a33bed5d45d4b894a2e87641db25855b23a
miluvalverde <miluvalverde11@gmail.com>
2022-11-21 19:00:47 -0300

    Add class Piscina in gimnasio to clase 10

---
+++ POO/src/clase10/gimnasio/Piscina.java
@@ -0,0 +1,23 @@
+package clase10.gimnasio;
+
+public class Piscina {
+
+    // Atributos
+    private double costoMensual;
+
+
+    // Constructor
+    public Piscina(double costoMensual) {
+        this.costoMensual = costoMensual;
+    }
+
+
+    // Setter y getter
+    public double getCostoMensual() {
+        return costoMensual;
+    }
+
+    public void setCostoMensual(double costoMensual) {
+        this.costoMensual = costoMensual;
+    }
+}
