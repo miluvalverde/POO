971db8cb5c14ce011b397f54d75df0a69cdc157c
miluvalverde <miluvalverde11@gmail.com>
2022-11-23 21:46:23 -0300

    Add class DemoBanco to file banco in clase 11

---
+++ POO/src/clase11/banco/DemoBanco.java
@@ -0,0 +1,28 @@
+package clase11.banco;
+
+public class DemoBanco {
+
+    public static void main(String[] args) {
+
+        // Cliente
+        Cliente cliente1 = new Cliente(234, "Valverde", 4563348);
+
+
+        // Cuenta de ahorro
+        CuentaAhorro cuentaAhorro1 = new CuentaAhorro(3440);
+        cuentaAhorro1.setCliente(cliente1);
+        cuentaAhorro1.cobrarInteres();
+        cuentaAhorro1.depositar(400);
+        cuentaAhorro1.extraer(30);
+
+
+        // Cuenta corriente
+        CuentaCorriente cuentaCorriente1 = new CuentaCorriente(4500);
+        cuentaCorriente1.extraer(5000);
+        cuentaCorriente1.setCliente(cliente1);
+
+        // Asigno las cuentas al cliente
+        cliente1.setCuentas(new Cuenta[]{cuentaAhorro1, cuentaCorriente1});
+    }
+
+}
