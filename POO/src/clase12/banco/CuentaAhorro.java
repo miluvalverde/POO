90e60c3b5ad53ed06734307c07939181c2b6acf3
miluvalverde <miluvalverde11@gmail.com>
2022-11-25 18:48:36 -0300

    Add class CuentaAhorro to folder banco in clase 12

---
+++ POO/src/clase12/banco/CuentaAhorro.java
@@ -0,0 +1,40 @@
+package clase12.banco;
+
+public class CuentaAhorro extends Cuenta {
+
+    // Aributos
+    private double tasaInteres = 1.5;
+
+
+    // Constructor
+    public CuentaAhorro(double saldo) {
+        super(saldo);
+    }
+
+    // Métodos
+    public void cobrarInteres() {
+        this.saldo += this.saldo / 100 * this.tasaInteres;
+    }
+
+    @Override
+    public void depositar(double monto) {
+        this.saldo += monto;
+        System.out.println("Se ha depositado a la cuenta. Su saldo es de: $" + this.saldo);
+    }
+
+    @Override
+    public void extraer(double monto) {
+        this.saldo -= monto;
+        System.out.println("Se ha extraido a la cuenta. Su saldo es de: $" + this.saldo);
+    }
+
+
+    // Setters y getters
+    public double getTasaInteres() {
+        return tasaInteres;
+    }
+
+    public void setTasaInteres(double tasaInteres) {
+        this.tasaInteres = tasaInteres;
+    }
+}
