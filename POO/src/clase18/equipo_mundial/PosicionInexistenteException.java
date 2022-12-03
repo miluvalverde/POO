358b02be654b9495fe8c2c2c4f55fbba08687907
miluvalverde <miluvalverde11@gmail.com>
2022-12-03 11:26:22 -0300

    Add class PosicionInexistenteException to equipo mundial in clase 18

---
+++ POO/src/clase18/equipo_mundial/PosicionInexistenteException.java
@@ -0,0 +1,10 @@
+package clase18.equipo_mundial;
+
+public class PosicionInexistenteException extends Exception{
+
+    // Constructor
+    public PosicionInexistenteException() {
+        super("Posición inexistente");
+    }
+
+}
