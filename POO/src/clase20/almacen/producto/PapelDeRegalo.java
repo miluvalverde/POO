73a64b8a4e3dec1afeefecea37a17565579a485e
miluvalverde <miluvalverde11@gmail.com>
2022-12-10 11:48:31 -0300

    Add class PapelDeRegalo to almacen in clase 20

---
+++ POO/src/clase20/almacen/producto/PapelDeRegalo.java
@@ -0,0 +1,10 @@
+package clase20.almacen.producto;
+
+public class PapelDeRegalo extends Producto {
+
+    // Método
+    @Override
+    public double calcularEspacio() {
+        return 0;
+    }
+}
