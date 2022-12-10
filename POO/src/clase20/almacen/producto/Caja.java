2e5ab77df4456fa236ebd838c7213c8948103051
miluvalverde <miluvalverde11@gmail.com>
2022-12-10 11:47:48 -0300

    Add class Caja to almacen in clase 20

---
+++ POO/src/clase20/almacen/producto/Caja.java
@@ -0,0 +1,53 @@
+package clase20.almacen.producto;
+
+public class Caja extends Producto {
+
+    // Atributos
+    private double longitud;
+    private double ancho;
+    private double altura;
+
+
+    // Constructor
+    public Caja() {
+    }
+
+    public Caja(double longitud, double ancho, double altura) {
+        this.longitud = longitud;
+        this.ancho = ancho;
+        this.altura = altura;
+    }
+
+
+    // Métodos
+    @Override
+    public double calcularEspacio() {
+        return this.longitud * this.altura * this.ancho;
+    }
+
+
+    // Getters y setters
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
+
+    public double getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(double ancho) {
+        this.ancho = ancho;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+}
