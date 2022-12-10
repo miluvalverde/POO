836f22aefb434f34e673fa3f240e26d6aa61a355
miluvalverde <miluvalverde11@gmail.com>
2022-12-10 11:48:42 -0300

    Add class Pelota to almacen in clase 20

---
+++ POO/src/clase20/almacen/producto/Pelota.java
@@ -0,0 +1,32 @@
+package clase20.almacen.producto;
+
+public class Pelota extends Producto {
+
+    // Atributos
+    private double radio;
+
+
+    // Constructor
+    public Pelota() {
+    }
+
+    public Pelota(double radio) {
+        this.radio = radio;
+    }
+
+    // Métodos
+    @Override
+    public double calcularEspacio() {
+        return (4 / 3) * Math.PI * Math.pow(this.radio, 3);
+    }
+
+
+    // Getters y setters
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+}
